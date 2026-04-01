a404b7d2d3fa604151d9bfe6873dd3053eedc9bf
Afsana Fazil <afsanafazil03@gmail.com>
2026-04-02 00:27:09 +0530

    fix cloudinary url for rating images

--- src/udemy/utils/fields.py
+++ src/udemy/utils/fields.py
@@ -2,12 +2,6 @@ from rest_framework import serializers
 
 CLOUD_NAME = "djptbf0iw"
 
-try:
-    from .cloudinary_map import CLOUDINARY_MAP
-except ImportError:
-    CLOUDINARY_MAP = {}
-
-
 class CloudinaryURLField(serializers.Field):
 
     def __init__(self, resource_type="image", *args, **kwargs):
@@ -23,10 +17,6 @@ class CloudinaryURLField(serializers.Field):
         if url.startswith("http"):
             return url
 
-        filename = url.split("/")[-1].split(".")[0]
-
-        if filename in CLOUDINARY_MAP:
-            public_id = CLOUDINARY_MAP[filename]
-            return f"https://res.cloudinary.com/{CLOUD_NAME}/{self.resource_type}/upload/{public_id}"
+        public_id = url.rsplit(".", 1)[0]
         
-        return f"https://res.cloudinary.com/{CLOUD_NAME}/{self.resource_type}/upload/{url}"
+        return f"https://res.cloudinary.com/{CLOUD_NAME}/{self.resource_type}/upload/{public_id}"
